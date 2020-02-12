ede0729c33caa422c134405a71511177a68653b8
rjcrter11 <rjcrter11@gmail.com>
2020-02-12 16:25:57 -0600

    added a couple routes

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home-wrapper">
+      <img
+        className="home-img"
+        src="https://images.unsplash.com/photo-1576520804567-b88e5dade12d?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&dl=harrison-kugler-GI1oheiVl9c-unsplash.jpg"
+        alt="crytocurrency"
+      />
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDarkMode } from "../hooks/useDarkMode";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode("darkMode", false);
@@ -10,6 +11,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
+      <div className="links ">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/coins">Coins</NavLink>
+      </div>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
